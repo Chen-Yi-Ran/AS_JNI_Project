a587d5157952682bda85784be476c6fe1b041ad3
chenyiran <chenyiran@stary.com>
2026-03-18 22:08:56 +0800

    模拟变声功能

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,147 +1,144 @@
-#include <jni.h>
-#include <string>
-#include "iostream"
 
-//NDK工具链里面的Log库
-#include <android/log.h>
-#define TAG  "LOGAN"
-//...表示不知道传入什么，借助JNI里的宏来自动帮我填充
-#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG,TAG,__VA_ARGS__)
-#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR,TAG,__VA_ARGS__)
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO,TAG,__VA_ARGS__)
+#include <unistd.h>
+#include "com.example.as_jni_project.MainActivity.h"
 
-
-
-extern "C" //全部采用C的方式，不准你的函数重载，函数名一样的问题
-JNIEXPORT //标记该方法可以被外部调用
-jstring  //Java<----->native 转换用的
-JNICALL //代表JNI标记
-//JNIEnv* env JNI的桥梁环境 300多个函数，所有JNI操作
-//jobject 谁调用就是谁的实例 MainActivity this
-//jclass clazz 谁调用，就是谁的class MainActivity.class
-//函数的实现
-Java_com_example_as_1jni_1project_MainActivity_stringFromJNI(
-        JNIEnv* env,
-        jobject /* this */) {
-
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
-}
-
-//jint == int
-//jstring == String
-//jintArray == int[]
-//jobjectArray == 引用类型对象，例如String[] Test[] Student[] Person[]
+using namespace FMOD;//fmod的命名空间
 extern "C"
 JNIEXPORT void JNICALL
-Java_com_example_as_1jni_1project_MainActivity_testArrayAction(JNIEnv *env, jobject thiz,
-                                                               jint count,
-                                                               jstring text_indo,
-                                                               jintArray ints,
-                                                               jobjectArray strs) {
-    //基本数据类型 jint count,jstring text_info，最简单的
-    int countInt = count; //jint本质是int，所以可以用int接收
-    LOGI("参数一 countInt：%d\n",count);
+//方法实现
+Java_com_example_as_1jni_1project_MainActivity_voiceChangeNative(JNIEnv *env, jobject thiz,
+                                                                 jint mode, jstring path) {
+    char * content_ = "默认 播放完毕";
+    //C认识的字符串
+    const char * path_ = env->GetStringUTFChars(path,NULL);
 
-    //const char* GetStringUTFChars(jstring string,jboolean* isCopy)
-    const char* testInfo = env->GetStringUTFChars(text_indo,NULL);
-    LOGI("参数二 textInfo:%s\n",testInfo);
+    //音效引擎系统指针
+    System *system = 0;
 
-    // ② 把int[] 转成 int*
-    // jint* GetIntArrayElements(jintArray array, jboolean* isCopy)
-    int* jintArray = env->GetIntArrayElements(ints,NULL);
-    //Java层数组的长度
-    // jsize GetArrayLength(jarray array) // jintArray ints 可以放入到 jarray的参数中去
-    jsize size = env->GetArrayLength(ints);
-    for(int i = 0;i<size;i++){
-        *(jintArray+i)+=100;
-        LOGI("参数三 int[]:%d\n",*jintArray+i);
+    //声音 指针
+    Sound * sound = 0;
+
+    //通道，音轨，声音在上面跑
+    Channel* channel = 0;
+
+    //DSP 数字信号处理 指针
+    DSP * dsp = 0;
+
+    //第一步 创建系统
+    System_Create(&system);
+
+    //第二步 系统的初始化 参数1：最大音轨数 参数2：系统初始化标记 参数3:额外数据
+    system->init(32,FMOD_INIT_NORMAL,0);
+    //第三步 创建声音 参数1：路径 参数2：声音初始化标记 参数3：额外数据 参数4：声音指针
+    system->createSound(path_,FMOD_DEFAULT,0,&sound);
+    //第四步 播放声音 音轨 声音
+    //参数1：声音 参数2：分组音频 参数3：控制 参数4：通道
+    system->playSound(sound,0, false,&channel);
+
+    switch (mode) {
+        case com_example_as_jni_project_MainActivity_MODE_NORMAL: // 原生
+            content_ = "原生 播放完毕";
+            break;
+        case com_example_as_jni_project_MainActivity_MODE_LUOLI: // 萝莉
+            content_ = "萝莉 播放完毕";
+
+            // 音调高 -- 萝莉 2.0
+            // 1.创建DSP类型的Pitch 音调条件
+            system->createDSPByType(FMOD_DSP_TYPE_PITCHSHIFT, &dsp);
+            // 2.设置Pitch音调调节2.0
+            dsp->setParameterFloat(FMOD_DSP_PITCHSHIFT_PITCH, 2.0f);
+            // 3.添加音效进去 音轨
+            channel->addDSP(0, dsp);
+            break;
+        case com_example_as_jni_project_MainActivity_MODE_DASHU: // 大叔
+            content_ = "大叔 播放完毕";
+
+            // 音调低 -- 大叔 0.7
+            // 1.创建DSP类型的Pitch 音调条件
+            system->createDSPByType(FMOD_DSP_TYPE_PITCHSHIFT, &dsp);
+            // 2.设置Pitch音调调节2.0
+            dsp->setParameterFloat(FMOD_DSP_PITCHSHIFT_PITCH, 0.7f);
+            // 3.添加音效进去 音轨
+            channel->addDSP(0, dsp);
+            break;
+        case com_example_as_jni_project_MainActivity_MODE_GAOGUAI: // 搞怪
+            content_ = "搞怪 小黄人 播放完毕";
+
+            // 小黄人声音 频率快
+
+            // 从音轨拿 当前 频率
+            float mFrequency;
+            channel->getFrequency(&mFrequency);
+
+            // 修改频率
+            channel->setFrequency(mFrequency * 1.5f); // 频率加快  小黄人的声音
+            break;
+        case com_example_as_jni_project_MainActivity_MODE_JINGSONG: // 惊悚
+            content_ = "惊悚 播放完毕";
+
+            // 惊悚音效：特点： 很多声音的拼接
+
+            // TODO 音调低
+            // 音调低 -- 大叔 0.7
+            // 1.创建DSP类型的Pitch 音调条件
+            system->createDSPByType(FMOD_DSP_TYPE_PITCHSHIFT, &dsp);
+            // 2.设置Pitch音调调节2.0
+            dsp->setParameterFloat(FMOD_DSP_PITCHSHIFT_PITCH, 0.7f);
+            // 3.添加音效进去 音轨
+            channel->addDSP(0, dsp); // 第一个音轨
+
+            // TODO 搞点回声
+            // 回音 ECHO
+            system->createDSPByType(FMOD_DSP_TYPE_ECHO, &dsp);
+            dsp->setParameterFloat(FMOD_DSP_ECHO_DELAY, 200); // 回音 延时    to 5000.  Default = 500.
+            dsp->setParameterFloat(FMOD_DSP_ECHO_FEEDBACK, 10); // 回音 衰减度 Default = 50   0 完全衰减了
+            channel->addDSP(1,dsp); // 第二个音轨
+
+            // TODO 颤抖
+            // Tremolo 颤抖音 正常5    非常颤抖  20
+            system->createDSPByType(FMOD_DSP_TYPE_TREMOLO, &dsp);
+            dsp->setParameterFloat(FMOD_DSP_TREMOLO_FREQUENCY, 20); // 非常颤抖
+            dsp->setParameterFloat(FMOD_DSP_TREMOLO_SKEW, 0.8f); // ？？？
+            channel->addDSP(2, dsp); // 第三个音轨
+
+            // 调音师：才能跳出来  同学们自己去调
+            break;
+        case com_example_as_jni_project_MainActivity_MODE_KONGLING: // 空灵  学校广播
+            content_ = "空灵 播放完毕";
+
+            // 回音 ECHO
+            system->createDSPByType(FMOD_DSP_TYPE_ECHO, &dsp);
+            dsp->setParameterFloat(FMOD_DSP_ECHO_DELAY, 200); // 回音 延时    to 5000.  Default = 500.
+            dsp->setParameterFloat(FMOD_DSP_ECHO_FEEDBACK, 10); // 回音 衰减度 Default = 50   0 完全衰减了
+            channel->addDSP(0,dsp);
+            break;
+
     }
-    // 目前无法控制Java的数组 变化 +100
-    // 操作杆 ----&gt; JMV
-    // env-&gt;
 
-    /**
-     * 0:           刷新Java数组，并 释放C++层数组
-     * JNI_COMMIT:  只提交 只刷新Java数组，不释放C++层数组
-     * JNI_ABORT:   只释放C++层数组
-     */
-     env->ReleaseIntArrayElements(ints,jintArray,0);
+    //等待播放完毕再回收
+    bool isPlayer = true;
+    while (isPlayer){
+        channel->isPlaying(&isPlayer);// 如果真的播放完成了，音轨是知道的，内部会修改isPlayer=false
+        usleep(1000*1000);
+    }
 
+    //记得回收
+    sound->release();
+    system->close();
+    system->release();
+    env->ReleaseStringUTFChars(path,path_);
 
-     //jobjectArray代表是Java的引用类型数组，不一样
-     jsize strSize = env->GetArrayLength(strs);
-     for(int i = 0;i<strSize;i++){
-         jstring jobj = static_cast<jstring>(env->GetObjectArrayElement(strs, i));
-         // 模糊：isCopy内部启动的机制
-         // const char* GetStringUTFChars(jstring string, jboolean* isCopy)
-         const char * jobjCharp = env->GetStringUTFChars(jobj,NULL);
-         LOGI("参数四 引用类型String具体的：%s\n",jobjCharp);
-         //释放jString
-         env->ReleaseStringUTFChars(jobj,jobjCharp);
-     }
-}
+    //告知Java播放完毕
+    jclass mainCls = env->GetObjectClass(thiz);
+    jmethodID endMethod = env->GetMethodID(mainCls,"playerEnd","(Ljava/lang/String;)V");
+    jstring value = env->NewStringUTF(content_);
+    env->CallVoidMethod(thiz,endMethod,value);
 
 
-extern "C"
-JNIEXPORT void JNICALL
-Java_com_example_as_1jni_1project_MainActivity_putObject(JNIEnv *env,
-                                                         jobject thiz,
-                                                         jobject student,
-                                                         jstring str) {
-    const char* strChar = env->GetStringUTFChars(str,NULL);
-    LOGI("strChar:%s\n",strChar);
-    env->ReleaseStringUTFChars(str,strChar);
 
-    //---------
-    //1.寻找类Student
-    //jclass studentClass = env->FindClass("com/derry/as_jni_project/Student"); // 第一种
-    jclass studentClass = env->GetObjectClass(student); //第二种
 
-    //2.Student类里面的函数规则 签名
-    jmethodID  setName = env->GetMethodID(studentClass,"setName","(Ljava/lang/String;)V");
-    jmethodID getName = env->GetMethodID(studentClass,"getName","()Ljava/lang/String;");
-    jmethodID showInfo = env->GetStaticMethodID(studentClass,"showInfo","(Ljava/lang/String;)V");
 
-    //调用setName
-    jstring value = env->NewStringUTF("AAAA");
-    env->CallVoidMethod(student,setName,value);
 
-    //调用getName
-    jstring getNameResult = static_cast<jstring>(env->CallObjectMethod(student, getName));
-    const char * getNameValue = env->GetStringUTFChars(getNameResult,NULL);
-    LOGE("调用到getName方法，值是:%s\n",getNameValue);
 
-    //调用静态showInfo
-    jstring jstringValue = env->NewStringUTF("静态方法你好，我是C++");
-    env->CallStaticVoidMethod(studentClass,showInfo,jstringValue);
-}
 
-extern "C"
-JNIEXPORT void JNICALL
-Java_com_example_as_1jni_1project_MainActivity_insertObject(JNIEnv *env, jobject thiz) {
-
-    //1.通过包名+类名的方式，拿到Student class，凭空拿class
-    const char * studentStr = "com/example/as_jni_project/Student";
-    jclass studentClas = env->FindClass(studentStr);
-
-    //2.通过student的class实例化此Student对象 c++ new Student
-    jobject studentObj = env->AllocObject(studentClas);//AllocObject只实例化对象，不会调用对象的构造函数
-    //方法的签名规则
-    jmethodID setName = env->GetMethodID(studentClas,"setName","(Ljava/lang/String;)V");
-    jmethodID setAge = env->GetMethodID(studentClas,"setAge","(I)V");
-
-    //调用方法
-    jstring strValue = env->NewStringUTF("Logan");
-    env->CallVoidMethod(studentObj,setName,strValue);
-    env->CallVoidMethod(studentObj,setAge,99);
-    // env-&gt;NewObject() // NewObject 实例化对象，会调用对象的构造函数
-    // ====================  下面是 Person对象  调用person对象的  setStudent 函数等
-    //4.通过包名+类名的方式，拿到Student class 凭空拿class
-    const char *personstr = "com/example/as_jni_project/Person";
-    jclass personClass = env->FindClass(personstr);
-    jobject personObj  = env->AllocObject(personClass); //AllocObject 只实例化对象，不会调用对象的构造函数
-    //setStudent此函数的签名规则
-    jmethodID  setStudent = env->GetMethodID(personClass,"setStudent","(Lcom/example/as_jni_project/Student;)V");
-    env->CallVoidMethod(personObj,setStudent,studentObj);
 }
--- app/src/main/java/com/example/as_jni_project/MainActivity.java
+++ app/src/main/java/com/example/as_jni_project/MainActivity.java
@@ -5,69 +5,94 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
-import com.example.as_jni_project.databinding.ActivityMainBinding;
+import org.fmod.FMOD;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Used to load the 'as_jni_project' library on application startup.
+    private static final int MODE_NORMAL = 0; // 正常
+    private static final int MODE_LUOLI = 1; //
+    private static final int MODE_DASHU = 2; //
+    private static final int MODE_JINGSONG = 3; //
+    private static final int MODE_GAOGUAI = 4; //
+    private static final int MODE_KONGLING = 5; //
+    private String audioFilePath; // 真实文件路径
+
+    private String path;
+
     static {
-        System.loadLibrary("as_jni_project");
+        System.loadLibrary("native-lib");
     }
 
-    private ActivityMainBinding binding;
+    private void copyAssetToFiles(String assetName) {
+        try {
+            InputStream in = getAssets().open(assetName);
+            File outFile = new File(getFilesDir(), assetName);
+            audioFilePath = outFile.getAbsolutePath();
 
-    private final static String TAG = MainActivity.class.getSimpleName();
-
-    //交互操作 JNI
-    public native void testArrayAction(int count,String textIndo,int[] ints,String[] strs);
-    public native void putObject(Student student,String str);//传递引用类型，传递对象
-    public native void insertObject();//凭空创建java对象
-
-
-
-    //点击事件：操作testArrayAction函数
-    public void test01(View view){
-        int[] ints = new int[]{1,2,3,4,5,6};
-        String[] strs = new String[]{"李小龙","李连杰","李元霸"};
-        testArrayAction(99,"你好",ints,strs);
-        for (int aInt:ints){
-            Log.d(TAG,"test01:aint---"+aInt);
+            if (!outFile.exists()) {
+                OutputStream out = new FileOutputStream(outFile);
+                byte[] buffer = new byte[1024];
+                int read;
+                while ((read = in.read(buffer)) != -1) {
+                    out.write(buffer, 0, read);
+                }
+                out.close();
+            }
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
 
-    public void test02(View view){
-        Student student = new Student();
-        student.name = "史泰龙";
-        student.age = 88;
-        putObject(student, "九阳神功");
-    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        copyAssetToFiles("derry.mp3");
 
-
-    public void test03(View view){
-        insertObject();
-    }
-
-    public void test04(View view){
-
-    }
-
-    public void test05(View view){
+        FMOD.init(this);
 
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+    protected void onDestroy() {
+        super.onDestroy();
+        FMOD.close();
     }
 
-    /**
-     * A native method that is implemented by the 'as_jni_project' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+    public void onFix(View view) {
+        Log.d("cyr","打印一下audioFilePath----"+audioFilePath);
+        int id = view.getId();
+        if (id == R.id.btn_normal) {
+            voiceChangeNative(MODE_NORMAL, audioFilePath);
+        } else if (id == R.id.btn_luoli) {
+            voiceChangeNative(MODE_LUOLI, audioFilePath);
+        } else if (id == R.id.btn_dashu) {
+            voiceChangeNative(MODE_DASHU, audioFilePath);
+        } else if (id == R.id.btn_jingsong) {
+            voiceChangeNative(MODE_JINGSONG, audioFilePath);
+        } else if (id == R.id.btn_gaoguai) {
+            voiceChangeNative(MODE_GAOGUAI, audioFilePath);
+        } else if (id == R.id.btn_kongling) {
+            voiceChangeNative(MODE_KONGLING, audioFilePath);
+        }
+    }
+
+    public void playerEnd(String mes){
+        Toast.makeText(this,mes,Toast.LENGTH_LONG).show();
+    }
+
+    public native void voiceChangeNative(int modelNormal,String path);
+
 
 }
